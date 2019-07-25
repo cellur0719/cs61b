624a014105226f23cbeccc3b0451af288eabee6c
Shu Pan <shupan0719@gmail.com>
2019-07-24 20:35:32 -0500

    lab3

---
+++ lab3/Flik/testHS.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testHS {
+
+    @Test
+    public void testHS(){
+        Integer a = 128;
+        Integer b = 128;
+        assertTrue(a==b);
+    }
+}
